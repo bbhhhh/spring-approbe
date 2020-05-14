853272b2cd2a620c98f7e04c4deae360aa24ba6d
bhh <bbhhhh_1@163.com>
2020-05-14 15:41:14 +0800

    first commit

---
+++ src/main/java/com/nokia/itms/approbe/SpringApprobeApplication.java
@@ -0,0 +1,13 @@
+package com.nokia.itms.approbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApprobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApprobeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nokia/itms/approbe/SpringApprobeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nokia.itms.approbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApprobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
