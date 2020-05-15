6a0179c6893e820c3d7d9b4ea973a66f24ac92ed
bhh <bbhhhh_1@163.com>
2020-05-15 14:14:49 +0800

    update test file

--- src/test/java/com/nokia/itms/approbe/SpringApprobeApplicationTests.java
+++ src/test/java/com/nokia/itms/approbe/SpringApprobeApplicationTests.java
@@ -1,13 +1,12 @@
 package com.nokia.itms.approbe;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringApProbeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    //	@Test
+    //	void contextLoads() {
+    //	}
 
 }
